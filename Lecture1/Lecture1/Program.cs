0e0c3f2849c72eb384192c0e34a7be8106a1ec3b
Olivia Smith <olivia.smith@IntelliTect.com>
2022-01-11 16:09:36 -0800

    Add person class

--- Lecture1/Lecture1/Program.cs
+++ Lecture1/Lecture1/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main()
+    {
+        string text = "Inigo Montoya";
+        Guid guid = Guid.NewGuid();
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Inigo Montoya");
+
+    }
+}
+
