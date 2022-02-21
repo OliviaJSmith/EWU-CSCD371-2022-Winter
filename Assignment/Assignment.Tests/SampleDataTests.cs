1181368ea960c6696133eb0286ee025c1918f9bc
Jenny Meadowcroft <jenny.meadowcroft@intellitect.com>
2022-02-21 11:08:54 -0800

    Started assignment

---
+++ Assignment/Assignment.Tests/SampleDataTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment.Tests
+{
+    [TestClass]
+    public class SampleDataTests
+    {
+        public SampleDataTests()
+        {
+
+        }
+
+        [TestMethod]
+        public void Constructor_CsvRows_Success()
+        {
+
+        }
+    }
+}
--- Assignment/Assignment/SampleData.cs
+++ Assignment/Assignment/SampleData.cs
@@ -1,23 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace Assignment
 {
     public class SampleData : ISampleData
     {
         // 1.
-        public IEnumerable<string> CsvRows => throw new NotImplementedException();
+        public IEnumerable<string> CsvRows { get; private set; }
+
+        IEnumerable<IPerson> ISampleData.People => throw new NotImplementedException();
 
         // 2.
-        public IEnumerable<string> GetUniqueSortedListOfStatesGivenCsvRows() 
-            => throw new NotImplementedException();
+        public IEnumerable<string> GetUniqueSortedListOfStatesGivenCsvRows()
+        {
+            List<string> uniqueStates = new();
+            IEnumerable<string?> states = CsvRows.Select(l => (string?)l.Split(',').GetValue(7)).Distinct();
+            return uniqueStates;
+        }
 
         // 3.
         public string GetAggregateSortedListOfStatesUsingCsvRows()
             => throw new NotImplementedException();
 
         // 4.
-        public IEnumerable<IPerson> People => throw new NotImplementedException();
+        public IEnumerable<IPerson> People()
+        {
+            List<IPerson> people = new List<IPerson>();
+            List<string[]> CsvArrayRows = CsvRows.Select(l => l.Split(',')).ToList();
+
+            CsvArrayRows.ForEach(row =>
+            {
+                Address temp = new Address(row[4], row[5], row[6], row[7]);
+                people.Add(new Person(row[1], row[2], temp, row[3]));
+            });
+            return people;
+        }
 
         // 5.
         public IEnumerable<(string FirstName, string LastName)> FilterByEmailAddress(
@@ -26,5 +45,14 @@ namespace Assignment
         // 6.
         public string GetAggregateListOfStatesGivenPeopleCollection(
             IEnumerable<IPerson> people) => throw new NotImplementedException();
+
+        public SampleData()
+        {
+            string filename = "People.csv";
+            List<string> lines = File.ReadAllLines(filename).Skip(1).ToList();
+            CsvRows = lines;
+            //lines.Select(l => l.Split(',')).ToArray();
+
+        }
     }
 }
